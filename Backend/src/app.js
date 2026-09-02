08eb858dfc0a3a4db39cd20350bedab928208568
asmitmishra234 <asmit312mishra@gmail.com>
2026-09-02 23:29:15 +0530

    update production cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,12 +8,13 @@ import postRouter from "./routes/post.routes.js";
 const app=express()
 app.use(cookieParser());
 app.use(express.json())
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://instagram-8x5l.onrender.com",
     credentials: true,
   })
-);
+)
 
 
 app.use('/api/auth',appRouter)
